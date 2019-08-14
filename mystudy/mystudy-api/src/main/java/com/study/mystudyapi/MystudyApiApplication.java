604e1fd9503f9874ef6a0ae58778d584600880a0
wangdi020 <wangdi020@ke.com>
2019-08-14 09:55:46 +0800

    建立项目

---
+++ mystudy/mystudy-api/src/main/java/com/study/mystudyapi/MystudyApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.mystudyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystudyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystudyApiApplication.class, args);
+    }
+
+}
---
+++ mystudy/mystudy-utils/src/main/java/com/item/mystudyutils/MystudyUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.item.mystudyutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystudyUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystudyUtilsApplication.class, args);
+    }
+
+}
